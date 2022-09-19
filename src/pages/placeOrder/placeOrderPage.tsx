2f9f8c559b01941dd4c158df37b8b639be33c36c
Gary Zhang <gary.zhang@lifebyte.io>
2022-09-19 17:32:49 +1000

    Finish place order functionality

--- src/pages/placeOrder/placeOrderPage.tsx
+++ src/pages/placeOrder/placeOrderPage.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { Col, Row } from 'antd'
-import { PaymentForm, TheMainLayout } from '@/components'
+import { CheckOutCard, PaymentForm, TheMainLayout } from '@/components'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { placeOrder } from '@/redux/order/slice'
 
 export const PlaceOrderPage: React.FC = () => {
+  const jwt = useAppSelector((state) => state.user.token) as string
+  const loading = useAppSelector((state) => state.order.loading)
+  const order = useAppSelector((state) => state.order.currentOrder)
+
+  const dispatch = useAppDispatch()
+
   return (
     <TheMainLayout>
       <Row>
         <Col span={12}>
           <PaymentForm />
         </Col>
-        <Col span={12}>{/* <CheckOutCard /> */}</Col>
+        <Col span={12}>
+          <CheckOutCard
+            loading={loading}
+            order={order}
+            onCheckout={() => {
+              void dispatch(placeOrder({ jwt, orderId: order.id }))
+            }}
+          />
+        </Col>
       </Row>
     </TheMainLayout>
   )
