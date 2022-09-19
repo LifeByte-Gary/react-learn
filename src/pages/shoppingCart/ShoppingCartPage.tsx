36973a95432e4f38cd2e282aaf9381ac778d316b
Gary Zhang <gary.zhang@lifebyte.io>
2022-09-19 10:51:39 +1000

    Initialise shopping cart page

--- src/pages/shoppingCart/ShoppingCartPage.tsx
+++ src/pages/shoppingCart/ShoppingCartPage.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import styles from './ShoppingCartPage.module.css'
+import { TheMainLayout } from '@/components'
+import { Affix, Col, Row } from 'antd'
 
 export const ShoppingCartPage: React.FC = () => {
-  return <div>Shopping Cart</div>
+  return (
+    <TheMainLayout>
+      <Row>
+        {/* 购物车清单 */}
+        <Col span={16}>
+          <div className={styles.productListContainer}>{/* <ProductList /> */}</div>
+        </Col>
+        {/* 支付卡组件 */}
+        <Col span={8}>
+          <Affix>
+            <div className={styles.paymentCardContainer}>{/* <PaymentCard /> */}</div>
+          </Affix>
+        </Col>
+      </Row>
+    </TheMainLayout>
+  )
 }
